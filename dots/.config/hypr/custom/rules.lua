1d6575cf43e5340a37bf767ad0afae3d76b5b7f3
kimforee <keshavimperial@gmail.com>
2026-07-15 16:53:18 +0530

    style: override screen sharing indicator to not be pinned

--- dots/.config/hypr/custom/rules.lua
+++ dots/.config/hypr/custom/rules.lua
@@ -1,3 +1,6 @@
 -- You can put custom rules here
 -- Window/layer rules: https://wiki.hyprland.org/Configuring/Window-Rules/
 -- Workspace rules: https://wiki.hyprland.org/Configuring/Workspace-Rules/
+
+-- Override screen sharing indicator: do not pin it on all workspaces (allow it to stay on Chrome's workspace and hide properly)
+hl.window_rule({match = {title = ".*is sharing (a window|your screen).*" }, pin = false})
